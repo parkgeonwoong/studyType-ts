0cc1e118637e067e9114a00f25c566b40e245456
parkgeonwoong <hk77kh@gmail.com>
2022-11-08 17:49:08 +0900

    Narrowing & Assertion 진행 및 응용 해보기

--- index.ts
+++ index.ts
@@ -160,3 +160,38 @@ console.log("Q3: ", app3(700, false, "중"));
 console.log("Q3: ", app3(100, false, "상"));
 
 console.clear();
+
+/**
+ * Narrowing
+ */
+
+function testNarrow(x: number | string) {
+  let varArr: number[] = [];
+
+  if (typeof x === "number") {
+    varArr[0] = x;
+  }
+}
+
+// Q.
+// 숫자여러개를 array 자료에 저장해놨는데
+// 가끔 '4', '5' 이런 식의 문자타입의 숫자가 발견되고 있습니다.
+// 이걸 클리닝해주는 함수가 필요합니다.
+// 클리닝함수( ['1', 2, '3'] ) 이렇게 숫자와 문자가 섞인 array를 입력하면
+// [1,2,3] 이렇게 숫자로 깔끔하게 변환되어 나오는 클리닝함수를 만들어오고 타입지정까지 확실히 해보십시오.
+
+function app4(arr: (string | number)[]): number[] {
+  let newArr: number[] = [];
+
+  arr.forEach((item) => {
+    if (typeof item === "string") {
+      newArr.push(parseInt(item));
+    } else {
+      newArr.push(item);
+    }
+  });
+
+  return newArr;
+}
+
+console.log(app4(["1", 2, "3"]));
