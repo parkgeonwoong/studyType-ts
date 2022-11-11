f188ed7eac23cb0c05203724cf329c9a364589b2
parkgeonwoong <hk77kh@gmail.com>
2022-11-11 17:57:37 +0900

    class

--- index.js
+++ index.js
@@ -44,3 +44,37 @@ naver.forEach((item) => {
 //     }
 // }
 // new Class2('a', 'b')
+/**
+ * class  문제
+ */
+const drawDiv = document.querySelector(".draw");
+const drawBtn = document.querySelector(".drawBtn");
+let rect;
+if (drawDiv instanceof Element) {
+    rect = drawDiv === null || drawDiv === void 0 ? void 0 : drawDiv.getBoundingClientRect();
+}
+class Square {
+    constructor(a, b, c) {
+        this.width = a;
+        this.height = b;
+        this.color = c;
+    }
+    draw() {
+        const newDiv = document.createElement("div");
+        const x = Math.random() * rect.width;
+        const y = Math.random() * rect.height;
+        newDiv.style.left = `${x}px`;
+        newDiv.style.top = `${y}px`;
+        newDiv.style.width = `${this.width}px`;
+        newDiv.style.height = `${this.height}px`;
+        newDiv.style.backgroundColor = `${this.color}`;
+        newDiv.style.marginBottom = "10px";
+        if (drawDiv instanceof Element) {
+            drawDiv.appendChild(newDiv);
+        }
+    }
+}
+let 네모 = new Square(30, 30, "red");
+drawBtn === null || drawBtn === void 0 ? void 0 : drawBtn.addEventListener("click", () => {
+    네모.draw();
+});
--- src/oop.ts
+++ src/oop.ts
@@ -5,7 +5,7 @@
 
 export {};
 
-class User {
+class User1 {
   name: string;
   private familyName: string = "park";
 
@@ -18,7 +18,7 @@ class User {
   }
 }
 
-const user1 = new User("woong");
+const user1 = new User1("woong");
 console.log(user1);
 
 user1.setFamilyName("Lee");
@@ -45,3 +45,127 @@ console.log(per1);
 
 per1.setId(255);
 console.log(per1);
+
+console.clear();
+
+/**
+ * protect
+ */
+
+class Pro {
+  protected name: string;
+}
+
+class Pro1 extends Pro {
+  app() {
+    this.name = "kim";
+  }
+}
+
+const copyPro = new Pro1();
+console.log(copyPro);
+
+/**
+ * static
+ */
+
+class sta {
+  static x: number = 10;
+}
+
+const copySta = new sta();
+console.log(sta.x);
+
+console.clear();
+
+class User2 {
+  static skill = "js";
+  intro = User2.skill + " world";
+}
+
+const ne = new User2();
+console.log(ne);
+
+User2.skill = "ts";
+console.log(ne);
+const ne2 = new User2();
+console.log(ne2);
+
+console.clear();
+
+// Q.
+// 다음 x, y, z 속성의 특징을 설명해보십시오.
+// class User {
+//   private static x = 10;
+//   public static y = 20;
+//   protected z = 30;
+// }
+// 누가 쓸 수 있고, 어디서 수정할 수 있는지 이런 것들이요.
+// 친구가 물어봤을 때 어떻게 답해줄 것입니까
+
+class knock {
+  private static x = 10;
+
+  tv() {
+    knock.x = 5;
+  }
+}
+
+const copyknock = new knock();
+console.log(knock);
+
+copyknock.tv();
+console.log(knock);
+
+const ab = new knock();
+console.log(knock);
+
+console.clear();
+
+///
+class d {
+  public static x = 10;
+
+  static df() {
+    d.x = 5;
+  }
+}
+
+const dd = new d();
+console.log(d);
+console.log(dd);
+d.df();
+console.log(d);
+
+console.clear();
+
+// Q.
+// x 속성에 숫자를 더해주는 함수가 필요합니다.
+// class User {
+//     private static x = 10;
+//     public static y = 20;
+//   }
+//   User.addOne(3) //이렇게 하면 x가 3 더해져야함
+//   User.addOne(4) //이렇게 하면 x가 4 더해져야함
+//   User.printX()  //이렇게 하면 콘솔창에 x값이 출력되어야함
+//   저렇게 User.addOne() 쓸 때마다 x가 증가하는 함수는 어떻게 만들 수 있을까요?
+//   그리고 x값을 콘솔창에 출력해주는 printX() 함수도 한번 만들어보십시오.
+//   (조건) private static x = 10; 이 코드 수정금지
+
+class User {
+  private static x = 10;
+  public static y = 20;
+
+  static addOne(x: number) {
+    User.x += x;
+  }
+
+  static printX() {
+    console.log(User.x);
+  }
+}
+
+// const test5 = new User()
+User.printX();
+User.addOne(5);
+User.printX();
