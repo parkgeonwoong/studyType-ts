30b9d83f85c8cf7d480e422dfe0e3877b3e6da29
parkgeonwoong <hk77kh@gmail.com>
2022-11-11 09:34:19 +0900

    Interface 문제 풀기

--- src/interface.js
+++ src/interface.js
@@ -7,3 +7,21 @@ let teacher = {
     name: "b",
     age: 20,
 };
+let 상품 = {
+    brand: "Samsung",
+    serialNumber: 1360,
+    model: ["TV", "phone"],
+};
+let 장바구니 = [
+    { product: "청소기", price: 7000 },
+    { product: "삼다수", price: 800 },
+];
+let t1 = [{ product: "청소기", price: 7000, card: false }];
+let objTest = {
+    plus(a, b) {
+        return a + b;
+    },
+    minus(a, b) {
+        return a - b;
+    },
+};
--- src/interface.ts
+++ src/interface.ts
@@ -20,3 +20,59 @@ let teacher: NewName = {
   name: "b",
   age: 20,
 };
+
+// Q.
+// interface 이용해서 간단하게 타입을 만들어봅시다
+interface TypeItem {
+  brand: string;
+  serialNumber: number;
+  model: string[];
+}
+
+let 상품: TypeItem = {
+  brand: "Samsung",
+  serialNumber: 1360,
+  model: ["TV", "phone"],
+};
+
+// Q.
+// 인터페이스는 오브젝트인데 배열로 어떻게 나타내는가?
+
+interface Basket {
+  product: string;
+  price: number;
+}
+
+interface ArrBasket extends Array<Basket> {}
+
+let 장바구니: Basket[] = [
+  { product: "청소기", price: 7000 },
+  { product: "삼다수", price: 800 },
+];
+
+interface T1 extends Basket {
+  card: boolean;
+}
+
+let t1: T1[] = [{ product: "청소기", price: 7000, card: false }];
+
+// Q.
+// object 안에 함수를 2개 넣고 싶은데요
+// 1. 이 object 자료는 plus() 함수를 내부에 가지고 있으며 plus 함수는 파라미터 2개를 입력하면 더해서 return 해줍니다.
+// 2. 이 object 자료는 minus() 함수를 내부에 가지고 있으며 minus 함수는 파라미터 2개를 입력하면 빼서 return 해줍니다.
+// 이 object 자료를 어떻게 만들면 될까요?
+// interface를 이용해서 object에 타입지정도 해보십시오.
+
+interface InfObj {
+  plus(a: number, b: number): number;
+  minus(a: number, b: number): number;
+}
+
+let objTest: InfObj = {
+  plus(a, b) {
+    return a + b;
+  },
+  minus(a, b) {
+    return a - b;
+  },
+};
