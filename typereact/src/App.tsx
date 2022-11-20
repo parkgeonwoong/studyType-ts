9061f5aab043234fad8a3f8cf9187e475bd3274e
parkgeonwoong <hk77kh@gmail.com>
2022-11-20 15:41:18 +0900

    Redux-toolkit TypeScript

--- typereact/src/App.tsx
+++ typereact/src/App.tsx
@@ -1,4 +1,7 @@
+import { Dispatch } from "@reduxjs/toolkit";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, RootState } from ".";
 import "./App.css";
 
 let Box: JSX.Element = <div></div>;
@@ -6,10 +9,15 @@ let Box: JSX.Element = <div></div>;
 function App() {
   const [user, setUser] = useState<string | number>("park");
 
+  const selector = useSelector((state: RootState) => state);
+  const dispatch: Dispatch = useDispatch();
+
   return (
     <div>
       <h2>Hello Type</h2>
       <Profile name="abc"></Profile>
+      <div>{selector.counterSlice.count}</div>
+      <button onClick={() => dispatch(increment())}>btn</button>
     </div>
   );
 }
--- typereact/src/index.tsx
+++ typereact/src/index.tsx
@@ -1,16 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createSlice, configureStore, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = { count: 0, user: "park" };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.count += 1;
+    },
+    decrement(state) {
+      state.count -= 1;
+    },
+    incrementByAmount(state, action: PayloadAction<number>) {
+      state.count += action.payload;
+    },
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+const store = configureStore({
+  reducer: {
+    counterSlice: counterSlice.reducer,
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
